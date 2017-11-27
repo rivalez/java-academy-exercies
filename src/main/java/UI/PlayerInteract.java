8432890187bca7995b4722636b8e1d2953d17c68
rivalez <marektabor1@gmail.com>
2017-11-27 16:24:50 +0100

    fix stackoverflow

--- src/main/java/UI/PlayerInteract.java
+++ src/main/java/UI/PlayerInteract.java
@@ -9,7 +9,6 @@ import java.util.logging.Logger;
 public class PlayerInteract {
 
     private Logger logger = Logger.getLogger(String.valueOf(PlayerInteract.class));
-    private Scanner scanner = new Scanner(System.in, "UTF-8");
     private CommunicateProvider communicateProvider;
     private Output output;
 
@@ -18,26 +17,30 @@ public class PlayerInteract {
         this.communicateProvider = communicateProvider;
     }
 
-    public int askForPosition(){
-        output.display(communicateProvider.getCommunicate(Communicate.SYMBOL));
+    public int askForPosition() {
         int i = 0;
         try {
+            Scanner scanner = new Scanner(System.in, "UTF-8");
+            output.display(communicateProvider.getCommunicate(Communicate.SYMBOL));
             i = scanner.nextInt();
         } catch (Exception e){
             logger.warning(e.getMessage());
             output.display(communicateProvider.getCommunicate(Communicate.WRONG_GLOBAL));
+            askForPosition();
         }
         return i;
     }
 
-    public GameSymbol askForSymbol(){
-        output.display(communicateProvider.getCommunicate(Communicate.WHO_START));
-        String symbol = scanner.next();
+    public GameSymbol askForSymbol() {
         GameSymbol gameSymbol = GameSymbol.X;
         try {
+            Scanner scanner = new Scanner(System.in, "UTF-8");
+            output.display(communicateProvider.getCommunicate(Communicate.WHO_START));
+            String symbol = scanner.next();
             gameSymbol = GameSymbol.valueOf(symbol.toUpperCase());
         } catch (Exception e){
             logger.warning(communicateProvider.getCommunicate(Communicate.WRONG_GLOBAL));
+            askForSymbol();
         }
         return gameSymbol;
     }
